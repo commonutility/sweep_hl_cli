94ef0ef5548d7aa3e39f4dda18f119cd39edbd11
commonutility <eric.w.zhu@gmail.com>
2025-05-25 23:46:30 -0700

    This works and claude-4-opus is goated

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="left-panel"></div>
+      <div className="main-panel"></div>
+      <div className="right-panel"></div>
+    </div>
+  )
+}
+
+export default App
