4a43b28ce20dbd67e74fafb22e1debc2806a2fc2
commonutility <eric.w.zhu@gmail.com>
2025-05-26 00:29:41 -0700

    before chatbox resizing

--- frontend/src/components/layout/RightPanel.jsx
+++ frontend/src/components/layout/RightPanel.jsx
@@ -3,7 +3,16 @@ import './RightPanel.css'
 const RightPanel = () => {
   return (
     <div className="right-panel">
-      {/* Right panel content will go here */}
+      <div className="right-panel-content">
+        {/* Right panel content will go here */}
+      </div>
+      <div className="right-panel-chatbox-container">
+        <input 
+          type="text" 
+          className="right-panel-chatbox"
+          placeholder="Type a message..."
+        />
+      </div>
     </div>
   )
 }
